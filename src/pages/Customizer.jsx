9aecbfb56ca54697e5d1d16d3e85e5a4ea130575
McBryan-dev <bryanokononfua@gmail.com>
2024-01-25 17:07:18 +0000

    Next Commit

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -29,6 +29,76 @@ const Customizer = () => {
 
     const snap = useSnapshot(state);
 
+    const [file, setFile] = useState('')
+
+    const [prompt, setPrompt] = useState('')
+
+    const [generatingImg, setGeneratingImg] = useState(false)
+
+    const [activeEditorTab, setActiveEditorTab] = useState('')
+
+    const [activeFilterTab, setActiveFilterTab] = useState({
+        logoShirt: true,
+        stylishShirt: false
+    })
+
+    const handleDecals = (type, result) => {
+        const decalType = DecalTypes[type];
+
+        state[decalType.stateProperty] = result;
+
+        if(activeFilterTab[decalType.filterTab]) {
+            handleActiveFilterTab(decalType.filterTab)
+        }
+    }
+
+    const handleActiveFilterTab = (tabName) => {
+        switch(tabName) {
+            case "logoShirt": 
+                state.isLogoTexture= !activeFilterTab[tabName];
+             break;
+            
+            case "stylishShirt": 
+                state.isFullTexture = !activeFilterTab[tabName];
+
+            default:
+                state.isFullTexture = false;
+                state.isLogoTexture = true;
+        }
+    }
+
+    const readFile = (type) => {
+        reader(file)
+         .then((result) => {
+            handleDecals(type, result)
+            setActiveEditorTab('')
+         })
+    }
+
+    //show tab contents depending on the active tab
+
+    const generateTabContent = () => {
+
+        switch(activeEditorTab) {
+            case "colorpicker": 
+                return <ColorPicker />
+
+            case "filepicker": 
+                return <FilePicker
+                    file={file}
+                    setFile={setFile}
+                    readFile={readFile}
+                />
+
+            case "aipicker":
+                return <AIPicker />
+
+            default: 
+                return null;
+        }
+
+    }
+
     return (
         <AnimatePresence>
             {!snap.intro && (
@@ -44,9 +114,14 @@ const Customizer = () => {
                                     <Tab 
                                         key={tab.name}
                                         tab={tab}
-                                        handleClick={() => {}}
+                                        handleClick={() => {
+                                            setActiveEditorTab(tab.name)
+                                            
+                                        }}
                                     />
                                 ))}
+
+                                {generateTabContent()}
                             </div>
                         </div>
 
@@ -68,7 +143,7 @@ const Customizer = () => {
                                     key= {tab.name}
                                     tab= {tab}
                                     isFilterTab
-                                    isActiveTab= " "
+                                    isActiveTab=""
                                     handleClick= {() => {}}
                                 />
                             ))

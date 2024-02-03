f23e4f2c41fb5b249457d07d853f58b33656d2b2
McBryan-dev <bryanokononfua@gmail.com>
2024-02-03 11:30:28 +0000

    Next Commit

--- src/components/AIPicker.jsx
+++ src/components/AIPicker.jsx
@@ -7,39 +7,8 @@ const AIPicker = ({prompt, setPrompt, generatingImg, handleSubmit}) => {
     return (
         <div className="aipicker-container">
 
-            <textarea 
-                className="aipicker-textarea"
-                rows={5}
-                value={prompt}
-                onChange={(e) => setPrompt(e.target.value)}
-                placeholder="Ask AI for an image."
-            />
-
-            <div className="flex flex-wrap gap-3">
-                {
-                    generatingImg ? (
-                        <CustomButton
-                            type="outline"
-                            title="Asking Ai.."
-                            customStyles="text-xs"
-                        />
-                    ) : <>
-                        <CustomButton
-                            type="outline"
-                            title="AI Logo"
-                            handleClick={() => handleSubmit('logo')}
-                            customStyles="text-xs"
-                        />
-
-                        <CustomButton
-                            type="filled"
-                            title="AI Full"
-                            handleClick={() => handleSubmit('full')}
-                            customStyles="text-xs"
-                        />
-                    </>
-                }
-            </div>
+            
+            AI_Picker
 
         </div>
     )
